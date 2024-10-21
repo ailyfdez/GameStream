218c357feb7cf4c9c3d1c5ae5dbf4d75f33ef70c
Arnaldo Pedrero <arnaldo.pedrero@gmail.com>
2024-10-21 16:53:49 -0300

    add init and registry buttons

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 19/255, green: 30/255, blue: 53/255,opacity: 1)
+            
+            VStack{
+                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.bottom , 60)
+                InitAndRegisterView()
+            }
         }
-        .padding()
+    }
+}
+ 
+
+struct InitAndRegisterView:View {
+    @State var initSessionType = true
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Spacer()
+                Button("INICIA SESSION"){
+                    initSessionType=true
+                    print("inicio")
+                }.foregroundColor(initSessionType ? .white : .gray)
+                Spacer()
+                Button("REGISTRATE"){
+                    initSessionType=false
+                    print("registro")
+                }.foregroundColor(initSessionType ? .gray : .white)
+                Spacer()
+            }
+            
+            Spacer(minLength: 42)
+            if(initSessionType==true){
+                InitSessionView()
+            }
+            else{
+                RegistryView()
+            }
+        }
+    }
+}
+
+struct InitSessionView:View{
+    var body: some View{
+        Text("inicio de seccion")
+    }
+}
+
+struct RegistryView:View{
+    var body: some View{
+        Text("Registro")
     }
 }
 
