2f5eb3c7d703f337bf8862f6ae6ff1de0337b7bd
ailyfdez <ailyfdez89@gmail.com>
2024-11-15 19:24:00 -0300

    finish video page

--- GameStream/Views/GameView.swift
+++ GameStream/Views/GameView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import AVKit
+import Kingfisher
 
 struct GameView: View {
     
@@ -32,6 +33,8 @@ struct GameView: View {
                     //Informacion Video
                     videoInfo(titulo:titulo,studio:studio,calificacion:calificacion,anoPublicacion:anoPublicacion,descripcion:descripcion,tags:tags)
                         .padding(.bottom)
+                    
+                    Galery(imgsUrl:imgsUrl)
                 }.frame( maxWidth: .infinity)
             }
         }
@@ -106,6 +109,107 @@ struct videoInfo:View {
     }
 }
 
+
+struct Galery:View {
+    
+    var imgsUrl:[String]
+    let formaGrid = [
+        GridItem(.flexible())
+    ]
+    
+    var body: some View{
+        VStack(alignment: .leading){
+            Text("GALERÍA").foregroundColor(.white).font(.title).padding(.leading)
+            ScrollView(.horizontal) {
+                LazyHGrid(rows: formaGrid, spacing: 8) {
+                    ForEach(imgsUrl, id: \.self) {
+                        imgUrl in
+                        
+                        KFImage(URL(string: imgUrl)).resizable()
+                            .aspectRatio(contentMode: .fill)
+                        
+                        
+                    }
+                    
+                }
+            }.frame( height: 180)
+            
+            extraViews()
+            
+            
+        }.frame( maxWidth: .infinity,alignment: .leading)
+        
+    }
+}
+
+struct extraViews:View {
+    var body: some View{
+        
+        VStack(alignment: .leading){
+            Text("COMENTARIOS").foregroundColor(.white).font(.title).padding(.leading)
+            
+            VStack{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("blue-gray"))
+                        .padding(.horizontal, 8.0)
+                    
+                    VStack(alignment: .leading) {
+                        HStack{
+                            Image( "perfil-ejemplo").resizable().aspectRatio(contentMode: .fit).frame(width: 64, height: 64).accentColor(.white).padding([.top, .leading])
+                            
+                            VStack(alignment: .leading) {
+                                Text("Lorem").bold().foregroundColor(Color.white)
+                                
+                                Text("Hace 18 horas").foregroundColor(Color.white)
+                            }
+                        }
+                        
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor, mauris lorem.  ").foregroundColor(.white).padding(.leading,8)
+                            .frame( height: 80)
+                    }
+                }
+                
+                ZStack{
+                    
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color("Blue-Gray"))
+                        .padding(.horizontal, 8.0)
+                    
+                    VStack(alignment: .leading) {
+                        HStack{
+                            
+                            Image( "perfil-ejemplo").resizable().aspectRatio(contentMode: .fit).frame(width: 64, height: 64).accentColor(.white).padding([.top, .leading])
+                            
+                            VStack(alignment: .leading) {
+                                Text("Ipsum").bold().foregroundColor(Color.white)
+                                
+                                Text("Hace 8 horas").foregroundColor(Color.white)
+                            }
+                        }
+                        
+                        Text("Vestibulum auctor, mauris lorem molestie nibh.Lorem ipsum consectetur adipiscing elit.    ").foregroundColor(.white)
+                            .padding(.horizontal, 8.0)
+                            .frame( height: 80)
+                    }
+                }
+            }
+            
+            
+            Text("JUEGOS SIMILARES").foregroundColor(.white).font(.title).padding(.leading)
+            
+            ScrollView(.horizontal,showsIndicators: false){
+                HStack{
+                    Image("abzu").resizable().scaledToFit().frame(width: 240, height: 135)
+                    
+                    Image("crash-bandicoot").resizable().scaledToFit().frame(width: 240, height: 135)
+                    
+                    Image("death-stranding").resizable().scaledToFit().frame(width: 240, height: 135)
+                }
+            }
+        }
+    }
+}
 
 #Preview {
     GameView(url: "Ejemplo.com", titulo: "Sonic", studio: "Sega", calificacion: "E+", anoPublicacion: "1991", descripcion: "Juego de Sega Genesis publicado en 1991 con más de 40 millones de copias vendidas actualmente", tags: ["plataformas","mascota"], imgsUrl: [ "https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg","https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg","https://cdn.cloudflare.steamstatic.com/steam/apps/268910/ss_615455299355eaf552c638c7ea5b24a8b46e02dd.600x338.jpg"])
